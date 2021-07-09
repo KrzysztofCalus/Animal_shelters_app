246f231761ecb3a9e90213ebc064da26ea5af050
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-07-09 18:46:19 +0200

    basic account view added

--- CL_final_project/urls.py
+++ CL_final_project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from animal_shelters.views import SignUpView
+from animal_shelters.views import SignUpView, AccountView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('account', AccountView.as_view(), name='account')
 ]
--- animal_shelters/views.py
+++ animal_shelters/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from animal_shelters.models import Owner
 
 # Create your views here.
+from django.views.generic import CreateView
+
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class AccountView(generic.CreateView):
+    model = Owner
+    fields = '__all__'
+    success_url = reverse_lazy('account')
+    template_name = 'owner_form.html'
